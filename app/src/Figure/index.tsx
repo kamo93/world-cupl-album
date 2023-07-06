1c5b8816b164b6fdb6bdbbb3f42e11a5293071ce
Kevin Andres Mancera Orjuela <93.kevinmancera@gmail.com>
2023-07-05 22:20:15 -0500

    Multiple fixes
    - Readme update and pririotize
    - remove danger color from buttons

--- app/src/Figure/index.tsx
+++ app/src/Figure/index.tsx
@@ -2,8 +2,12 @@ import { Block, Button } from 'react-bulma-components'
 import styled from 'styled-components'
 const REPEATXCOLOR = {
   0: { color: 'info', outlined: true },
-  1: { color: 'success', outlined: false },
-  2: { color: 'danger', outlined: false }
+  1: { color: 'info', outlined: false }
+}
+
+const REPEATXCOLORIMPORTANT = {
+  0: { color: 'warning', outlined: true },
+  1: { color: 'warning', outlined: false }
 }
 
 const FigureContainerStyled = styled(Block)`
@@ -23,6 +27,12 @@ const RepetedStyled = styled(Block)`
   z-index: 10;
 `
 
+const ButtonStyled = styled(Button)`
+  &:focus {
+    background-color: transparent;
+  }
+`
+
 interface FigureProps {
   albumNumber: string
   timesRepeat: number
@@ -30,28 +40,26 @@ interface FigureProps {
   onClick: React.MouseEventHandler<HTMLButtonElement>
 }
 
-function colorByTimesRepeated (timesRepeat: number) {
-  if (timesRepeat >= 2) {
-    return REPEATXCOLOR[2]
-  }
-  return REPEATXCOLOR[timesRepeat as 0 | 1]
+function colorByTimesRepeated (timesRepeat: number, colorDictionary: typeof REPEATXCOLOR) {
+  const controlTimesRepeated = timesRepeat >= 1 ? 1 : 0
+  return colorDictionary[controlTimesRepeated]
 }
 
 export default function Figure ({ albumNumber, timesRepeat, isImportant, onClick }: FigureProps) {
   let colors = {}
 
   if (isImportant) {
-    colors = { color: 'warning', outlined: true }
+    colors = colorByTimesRepeated(timesRepeat, REPEATXCOLORIMPORTANT)
   } else {
-    colors = colorByTimesRepeated(timesRepeat)
+    colors = colorByTimesRepeated(timesRepeat, REPEATXCOLOR)
   }
 
   return (
     <FigureContainerStyled m='0' p='1'>
       {timesRepeat >= 2 ? <RepetedStyled marginless textWeight='semibold' p='1'>+ {timesRepeat}</RepetedStyled> : null}
-      <Button textSize='6' textWeight='light' p='3' m='1' rounded {...colors} onClick={onClick}>
+      <ButtonStyled textSize='6' textWeight='light' p='3' m='1' rounded {...colors} onClick={onClick}>
         {albumNumber}
-      </Button>
+      </ButtonStyled>
     </FigureContainerStyled>
   )
 }
